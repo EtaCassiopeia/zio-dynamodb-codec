4de026981752b5eea3f541d1cb223fbc622dbdb8
Mohsen Zainalpour <zainalpour@gmail.com>
2026-04-09 17:45:19 -0400

    Add basic record derivation

--- schema-dynamodb/src/main/scala/zio/blocks/schema/dynamodb/DynamoDBCodecDeriver.scala
+++ schema-dynamodb/src/main/scala/zio/blocks/schema/dynamodb/DynamoDBCodecDeriver.scala
@@ -65,6 +65,10 @@ class DynamoDBCodecDeriver extends Deriver[DynamoDBCodec]:
       case e: Exception =>
         Left(SchemaError.expectationMismatch(Nil, s"Failed to parse '$s': ${e.getMessage}"))
 
+  @SuppressWarnings(Array("org.wartremover.warts.AsInstanceOf"))
+  private def resolveCodec[F[_, _]](meta: Any)(using hi: HasInstance[F]): DynamoDBCodec[Any] =
+    instance(meta.asInstanceOf[F[Any, Any]])(using hi).force.asInstanceOf[DynamoDBCodec[Any]]
+
   override def deriveRecord[F[_, _], A](
     fields: IndexedSeq[Term[F, A, ?]],
     typeId: TypeId[A],
@@ -74,7 +78,67 @@ class DynamoDBCodecDeriver extends Deriver[DynamoDBCodec]:
     defaultValue: Option[A],
     examples: Seq[A]
   )(implicit hasBinding: HasBinding[F], hasInstance: HasInstance[F]): Lazy[DynamoDBCodec[A]] =
-    throw new UnsupportedOperationException("Record derivation not yet implemented")
+    val fieldCount     = fields.size
+    val constructor    = binding.constructor
+    val deconstructor  = binding.deconstructor
+    val totalRegisters = constructor.usedRegisters
+
+    val bindingFields = fields.map { term =>
+      Term[Binding, A, Any](term.name, term.value.asInstanceOf[Reflect[Binding, Any]], term.doc, term.modifiers)
+    }
+    val bindingRecord = Reflect.Record[Binding, A](
+      bindingFields,
+      typeId,
+      binding,
+      doc,
+      modifiers,
+      None,
+      Seq.empty
+    )
+    val fieldRegisters = bindingRecord.registers
+
+    Lazy {
+      val fieldNames  = new Array[String](fieldCount)
+      val fieldCodecs = new Array[DynamoDBCodec[Any]](fieldCount)
+
+      var i = 0
+      while i < fieldCount do
+        fieldNames(i) = fields(i).name
+        fieldCodecs(i) = resolveCodec[F](fields(i).value.metadata)
+        i += 1
+
+      DynamoDBCodec.record[A](
+        enc = (value, output) =>
+          val regs = Registers(totalRegisters)
+          deconstructor.deconstruct(regs, 0, value)
+
+          var idx = 0
+          while idx < fieldCount do
+            val fieldVal = fieldRegisters(idx).asInstanceOf[Register[Any]].get(regs, 0)
+            val av       = fieldCodecs(idx).encodeValue(fieldVal)
+            output.put(fieldNames(idx), av)
+            idx += 1
+        ,
+        dec = input =>
+          val regs               = Registers(totalRegisters)
+          var idx                = 0
+          var error: SchemaError = null
+
+          while idx < fieldCount && error == null do
+            val raw = input.get(fieldNames(idx))
+            if raw == null then error = SchemaError.missingField(Nil, fieldNames(idx))
+            else
+              fieldCodecs(idx).decodeValue(raw) match
+                case Right(v) =>
+                  fieldRegisters(idx).asInstanceOf[Register[Any]].set(regs, 0, v)
+                case Left(e) =>
+                  error = e
+            idx += 1
+
+          if error != null then Left(error)
+          else Right(constructor.construct(regs, 0))
+      )
+    }
 
   override def deriveVariant[F[_, _], A](
     cases: IndexedSeq[Term[F, A, ?]],
